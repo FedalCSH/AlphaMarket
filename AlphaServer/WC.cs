8c737481fd4a41714158be0deb0f35ea1cfcfa9f
Алексей <fedalwork@gmail.com>
2023-03-27 12:33:31 +0300

    устанка мконок фонтавесом, устранены мелкие ошибки в верстке.

--- AlphaServer/WC.cs
+++ AlphaServer/WC.cs
@@ -7,7 +7,7 @@ namespace AlphaServer
 {
     public static class WC
     {
-        private const string imagePath = @"\images\product\";
+        private const string imagePath = @"/images/product/";
         public static string ImagePath => imagePath;
         public const string SessionCart = "ShoppingCartSession";
         public const string AdminRole = "Admin";
